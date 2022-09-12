47cef9fffb45ad5ee1ec3e1fceb3774d627c2b98
wolferl42195 <code@radinger-peer.at>
2022-09-12 20:19:01 +0200

    First test implemented

--- src/main/java/x.java
+++ src/main/java/x.java
@@ -1,1 +1,12 @@
-Place Holder
+
+public class x {
+
+    public int calc(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args ){
+        System.out.println("Hello world");
+    }
+
+}
